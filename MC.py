4cc8e2eb7b5cd1e42002b219a2683c7908e11c13
Duje <duje.maras@gmail.com>
2026-07-26 00:00:57 +0200

    Added Save/Load game state

--- MC.py
+++ MC.py
@@ -177,12 +177,54 @@ class Crafting:
     
         print(f"{naziv_predmeta} je uspjesno napravljen!")   
 
+def spremi_stanje(stats, inventar, path = "user.json"):
+    inventar_lista = []
+    
+    for predmet in inventar.predmeti:
+            if isinstance(predmet, Alat):
+                stavka = {"tip": "alat", "naziv": predmet.naziv, "kolicina": predmet.kolicina, "izdrzljivost": predmet.izdrzljivost}
+            elif isinstance(predmet, Hrana):
+                stavka = {"tip": "hrana", "naziv": predmet.naziv, "kolicina": predmet.kolicina, "siti": predmet.siti}
+            elif isinstance(predmet, Blok):
+                stavka = {"tip": "blok", "naziv": predmet.naziv, "kolicina": predmet.kolicina, "tvrdoca": predmet.tvrdoca}
+            inventar_lista.append(stavka)
+            
+    podaci = {
+        "health": stats.health,
+        "hunger": stats.hunger,
+        "inventar": inventar_lista
+    }
+    
+    with open(path, "w") as f:
+        json.dump(podaci, f, indent = 4)
+        
+def ucitaj_stanje(stats, inventar, path="user.json"):
+    with open(path, "r") as f:
+        podaci = json.load(f)
+    
+    stats.health = podaci["health"]
+    stats.hunger = podaci["hunger"]
+    
+    inventar.predmeti = []   
+    
+    for stavka in podaci["inventar"]:
+        if stavka["tip"] == "alat":
+            predmet = Alat(stavka["naziv"], kolicina=stavka["kolicina"], izdrzljivost=stavka["izdrzljivost"])
+        elif stavka["tip"] == "hrana":
+            predmet = Hrana(stavka["naziv"], kolicina=stavka["kolicina"], siti=stavka["siti"])
+        elif stavka["tip"] == "blok":
+            predmet = Blok(stavka["naziv"], kolicina=stavka["kolicina"], tvrdoca=stavka["tvrdoca"])
+        
+        inventar.predmeti.append(predmet)
+
 #Test
 moj_inventar = Inventar()
+
 moj_stats = Stats(moj_inventar)
-moj_stats.hunger = 10
 
-moj_inventar.dodaj_predmet(Hrana("Kruh", kolicina = 1, siti = 5))
-moj_inventar.pojedi("Kruh", moj_stats)
+ucitaj_stanje(moj_stats, moj_inventar)
 
-print(moj_stats)
+print(f"{moj_stats}\n")
+moj_inventar.prikazi_inventar()
+
+
