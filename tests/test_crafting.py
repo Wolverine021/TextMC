c16d282292205abf1fc69b113298782fda023419
Duje <duje.maras@gmail.com>
2026-08-12 15:16:26 +0200

    Added Crafting Tests

---
+++ tests/test_crafting.py
@@ -0,0 +1,48 @@
+import pytest
+from exceptions import RecepNePostoji, InventarPun, NedovoljnoMaterijala, MaterijalNepostoji
+from crafting import Crafting
+from inventory import Inventar
+from items import Blok, Alat
+
+def test_uspjesan_craft():
+    crafting = Crafting()
+    inventar = Inventar()
+    
+    inventar.dodaj_predmet(Blok("kamen", 3, 1))
+    inventar.dodaj_predmet(Blok("drvo", 2, 1))
+    
+    crafting.craft(inventar, "Pijuk")
+    
+    assert any(predmet.naziv == "Pijuk" for predmet in inventar.predmeti)
+    
+def test_recept_ne_postoji():
+    crafting = Crafting()
+    inventar = Inventar()
+    
+    with pytest.raises(RecepNePostoji):
+        crafting.craft(inventar, "Gijuk")
+        
+def test_pun_inventar():
+    inventar = Inventar(max_slotova=1)
+    
+    inventar.dodaj_predmet(Alat("Pijuk", 1, 100))
+    
+    with pytest.raises(InventarPun):
+        inventar.dodaj_predmet(Alat("Pijuk", 1, 100))
+
+def test_nedovoljno_materijala():
+    crafting = Crafting()
+    inventar = Inventar()
+    
+    inventar.dodaj_predmet(Blok("kamen", 2, 1))
+    inventar.dodaj_predmet(Blok("drvo", 2, 1))
+    
+    with pytest.raises(NedovoljnoMaterijala):
+        crafting.craft(inventar, "Pijuk")
+    
+def test_materijal_ne_postoji():
+    crafting = Crafting()
+    inventar = Inventar()
+    
+    with pytest.raises(MaterijalNepostoji):
+        crafting.craft(inventar, "Pijuk")
