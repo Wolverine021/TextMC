1ce8fd9c67ca26d7e2e0d1fba0e3e5167ae0f254
Duje <duje.maras@gmail.com>
2026-08-14 21:53:36 +0200

    Fixed main

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 from inventory import Inventar
 from crafting import Crafting
-from exceptions import InventarPun
+from items import Blok, Hrana
 from persistency import spremi_stanje, ucitaj_stanje
 from stats import Stats
-from items import Blok
 
+def nova_igra(inventar, stats):
+    inventar.predmeti.clear()
+
+    stats.health = 20
+    stats.hunger = 20
+
+    inventar.dodaj_predmet(Blok("drvo", 10, 1))
+    inventar.dodaj_predmet(Blok("kamen", 10, 2))
+    inventar.dodaj_predmet(Blok("psenica", 5, 1))
+    inventar.dodaj_predmet(Hrana("jabuka", 3, 1))
+
+    print("Nova igra je zapoceta!")
+    
+    
 def pokreni_igru():
     print("""
 ██████╗███████╗██╗  ██╗████████╗███╗   ███╗ ██████╗
@@ -21,11 +34,12 @@ def pokreni_igru():
     
     print("1. Ucitaj igru")
     print("2. Zapocni novu")
+    
     izbor = input("Izbor: ")
     if izbor == "1":
         ucitaj_stanje(stats, inventar)
     elif izbor == "2":
-        pass
+        nova_igra(inventar, stats)
     else:
         print("Nepoznat izbor...")
     
@@ -35,44 +49,40 @@ def pokreni_igru():
         print("3. Pojedi hranu")
         print("4. Koristi alat")
         print("5. Prikazi stats")
-        print("6. Stack Test")
-        print("7. Spremi i izadji")
+        print("6. Spremi i izadji")
         izbor = input("Izbor: ")
         
         if izbor == "1":
             inventar.prikazi_inventar()
+            
         elif izbor == "2":
             predmet = input("Izaberi predmet za napravit: ")   
             try:   
                 crafting.craft(inventar, predmet) 
             except Exception as e:
                 print(f"Greška: {e}")    
+        
         elif izbor == "3":
             hrana = input("Izaberi sto pojesti: ")
             try:
                 inventar.pojedi(hrana, stats)
             except Exception as e:
                 print(f"Greška {e}")
+        
         elif izbor == "4":
             alat = input("Izaberi alat za koristenje: ")
             try:
                 inventar.koristi_alat(alat)
             except Exception as e:
                 print(f"Greška: {e}")           
+        
         elif izbor == "5":
             print(stats)
+        
         elif izbor == "6":
-            try:
-                inventar.dodaj_predmet(Blok("Drvo", 30, 1))
-                inventar.dodaj_predmet(Blok("Drvo", 50, 1))
-                inventar.dodaj_predmet(Blok("Kamen", 140, 2))
-                inventar.prikazi_inventar()
-            except InventarPun as e:
-                print(f"Greška: {e}")
-
-        elif izbor == "7":
             spremi_stanje(stats, inventar)
             break
+        
         else:
             print("Nepoznat izbor...")
 
