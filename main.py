45bc99367423495b72e2cd1ed08650b02702184d
Duje <duje.maras@gmail.com>
2026-08-14 22:02:47 +0200

    Minor tweaks

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from stats import Stats
 def nova_igra(inventar, stats):
     inventar.predmeti.clear()
 
-    stats.health = 20
-    stats.hunger = 20
+    stats.health = stats.max_health
+    stats.hunger = stats.max_hunger
 
     inventar.dodaj_predmet(Blok("drvo", 10, 1))
     inventar.dodaj_predmet(Blok("kamen", 10, 2))
